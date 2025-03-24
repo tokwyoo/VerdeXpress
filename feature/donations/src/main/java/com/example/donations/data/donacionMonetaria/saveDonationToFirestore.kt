04cb802feca9d9ee372ac2ca31684688c1dcaf94
eli <ebmv03@gmail.com>
2025-03-24 09:18:35 -0700

    ps para guardar en el firestore

---
+++ feature/donations/src/main/java/com/example/donations/data/donacionMonetaria/saveDonationToFirestore.kt
@@ -0,0 +1,70 @@
+package com.example.donations.data.donacionMonetaria
+
+import android.util.Log
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import java.util.Calendar
+import java.util.TimeZone
+
+fun saveDonationToFirestore(
+    nombre: String,
+    correo: String,
+    numTel: String,
+    cantidad: String,
+    metodoPago: String,
+    parqueSeleccionado: String,
+    UbicacionSeleccionado: String,
+    quiereRecibo: Boolean,
+    rfc: String,
+    razon: String,
+    domFiscal: String,
+    cardBrand: String,
+    lastCardDigits: String,
+    transactionId: String,
+    transactionDate: String
+) {
+    val db = Firebase.firestore
+    val auth = FirebaseAuth.getInstance()
+
+    // Configuramos la zona horaria de Hermosillo
+    val timeZone = TimeZone.getTimeZone("America/Hermosillo")
+    val calendar = Calendar.getInstance(timeZone)
+
+    // Obtenemos el timestamp ajustado a la zona horaria de Hermosillo
+    val createdAt = calendar.time
+
+    // Datos de la donación
+    val donationData = hashMapOf(
+        "donante_nombre" to nombre,
+        "donante_contacto" to numTel,
+        "donante_correo" to correo,
+        "cantidad" to cantidad,
+        "metodo_pago" to metodoPago,
+        "parque_seleccionado" to parqueSeleccionado,
+        "ubicacion_seleccionada" to UbicacionSeleccionado,
+        "quiere_recibo" to quiereRecibo,
+        "rfc" to rfc,
+        "razon_social" to razon,
+        "domicilio_fiscal" to domFiscal,
+        "card_brand" to cardBrand,
+        "last_card_digits" to lastCardDigits,
+        "transaction_id" to transactionId,
+        "transaction_date" to transactionDate,
+        "registro_usuario" to auth.currentUser?.uid, // ID del usuario que realiza la donación
+        "created_at" to Timestamp(createdAt) // Timestamp ajustado a Hermosillo
+    )
+
+    // Guardar la donación en Firestore
+    db.collection("donaciones_monetaria")
+        .add(donationData)
+        .addOnSuccessListener {
+            // Éxito al guardar los datos
+            Log.d("Firestore", "Donación guardada correctamente con id: ${it.id}")
+        }
+        .addOnFailureListener { e ->
+            // Error al guardar los datos
+            Log.e("Firestore", "Error al guardar la donación: ${e.message}")
+        }
+}
